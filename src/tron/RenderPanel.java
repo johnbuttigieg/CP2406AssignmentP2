45a4598a7c886542941b154de52daaf68d71d2b2
johnbuttigieg <john.buttigieg@my.jcu.edu.au>
2017-11-01 13:17:22 +1000

    Created Render Panel Class
    This class allows me to render the panel into whichever colour I like

--- src/tron/RenderPanel.java
+++ src/tron/RenderPanel.java
@@ -1,4 +1,19 @@
 package tron;
 
-public class RenderPanel {
+import java.awt.*;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class RenderPanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(new Color(0));
+        g.fillRect(0, 0, 800, 700);
+        super.paintComponent(g);
+
+    }
+
+
 }
